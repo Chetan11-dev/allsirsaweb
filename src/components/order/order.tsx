164cc380a34c102de95b6e200d4315dc99c059ab
Chetan Jain <chetansirsa@gmail.com>
2020-06-28 20:43:35 +0530

    Modified Order

--- src/components/order/order.tsx
+++ src/components/order/order.tsx
@@ -1,14 +1,24 @@
 import React from 'react'
-import { ModelOrder } from './../../api/models/ModelOrder';
-import { beautify } from './../../utils/logutils';
+import { ModelOrder } from './../../api/models/ModelOrder'
+import { beautify } from './../../utils/logutils'
 interface Props {
-    order : ModelOrder , onChange:(m:ModelOrder) => void 
+    order: ModelOrder, onChange: (m: ModelOrder) => void
 }
-export const Order = ({onChange , order}:Props ) => {
-    
+export const Order = ({ onChange, order }: Props) => {
+
     return (
-        <div>
-            {beautify(order)}
+        <div className="container border border-primary">
+
+            <div>Hello</div>
+            <div>Hello</div>
+
+            <span>Hello</span>
+            <span>Hello</span>
+            <div className="row"> <span>Hello</span>
+                <div className="col-12 bg-dark ">.col-6 </div>
+            </div>
+
         </div>
+
     )
 }
